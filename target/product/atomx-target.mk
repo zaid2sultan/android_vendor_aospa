56604be5edde16f0593ba8d0d44ac12042f15be9
Christian Oder <myself5@carbonrom.org>
2021-01-05 12:31:37 -0300

    AtomX: target: config: Add basic hotword permissions
     * Required for older hotword blobs
    
    Change-Id: I3a978ddda791a00274064e47348953c46006ace2

--- target/product/atomx-target.mk
+++ target/product/atomx-target.mk
@@ -42,4 +42,5 @@ PRODUCT_VENDOR_MOVE_ENABLED := true
 # Permissions
 PRODUCT_COPY_FILES += \
     vendor/atomx/target/config/permissions/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml \
+    vendor/atomx/target/config/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml \
     vendor/atomx/target/config/permissions/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml
