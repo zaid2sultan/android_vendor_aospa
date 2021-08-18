c03007be1905d22ab7740051bee6b06ea19ea301
Vishalcj17 <vishalcj17@gmail.com>
2021-08-18 23:35:26 +0530

    AtomX: product: Include Qualcomm Common tree
    Change-Id: I671e6906e45abb78f5627af7784c01ce87283f46

--- target/product/atomx-target.mk
+++ target/product/atomx-target.mk
@@ -4,5 +4,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Include Common Qualcomm Device Tree
+$(call inherit-product, device/qcom/common/common.mk)
+
 # Include definitions for Snapdragon Clang
 $(call inherit-product, vendor/qcom/sdclang/config/SnapdragonClang.mk)
