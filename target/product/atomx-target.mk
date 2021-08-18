bd374804464f099c0b1afa65a70ab05d5845f80c
Vishalcj17 <vishalcj17@gmail.com>
2021-08-18 23:37:27 +0530

    AtomX: product: Include GMS, Modules and Pixel features
    Change-Id: I7f37f944e83237b9ae4b126bd796dfb1ed800029

--- target/product/atomx-target.mk
+++ target/product/atomx-target.mk
@@ -4,8 +4,16 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Don't dexpreopt prebuilts. (For GMS).
+DONT_DEXPREOPT_PREBUILTS := true
+
 # Include Common Qualcomm Device Tree
 $(call inherit-product, device/qcom/common/common.mk)
 
 # Include definitions for Snapdragon Clang
 $(call inherit-product, vendor/qcom/sdclang/config/SnapdragonClang.mk)
+
+# Include GMS, Modules and Pixel features
+$(call inherit-product, vendor/google/gms/config.mk)
+$(call inherit-product-if-exists, vendor/google/modules/build/mainline_modules_s.mk)
+$(call inherit-product, vendor/google/pixel/config.mk)
