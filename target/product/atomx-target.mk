82913edb261bf1d597969413f3c7b37591bef916
Vishalcj17 <vishalcj17@gmail.com>
2021-08-18 23:32:11 +0530

    AtomX: product: Include Snapdragon Clang makefile
    Change-Id: Ibb54342994005cdd43fc0f185e505573b6e3b6a3

---
+++ target/product/atomx-target.mk
@@ -0,0 +1,8 @@
+#
+# Copyright (C) 2022 The AtomX Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Include definitions for Snapdragon Clang
+$(call inherit-product, vendor/qcom/sdclang/config/SnapdragonClang.mk)
