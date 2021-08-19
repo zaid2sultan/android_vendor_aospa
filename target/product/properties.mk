c7e8a3cff5f3518896ac11757bb99d65556f5fc5
Vishalcj17 <vishalcj17@gmail.com>
2021-08-19 11:21:25 +0530

    AtomX: properties: Default to Gesture navigations
    Change-Id: I4673c5140494835eecf107368bc6bb5241adf67d

--- target/product/properties.mk
+++ target/product/properties.mk
@@ -4,6 +4,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Enable Navigation gestures.
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.boot.vendor.overlay.theme=com.android.internal.systemui.navbar.gestural
+
 # Enforce privapp-permissions whitelist.
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.control_privapp_permissions=enforce
