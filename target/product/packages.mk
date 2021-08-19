05bdaae8bda7dfb0797b9542b5c8250c61f19934
Vishalcj17 <vishalcj17@gmail.com>
2021-08-19 11:30:06 +0530

    AtomX: packages: Build HIDL base and manager
    Change-Id: I4775c26858f29f9f7f7ef5a618b36ef67048f5ea

--- target/product/packages.mk
+++ target/product/packages.mk
@@ -4,6 +4,13 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# HIDL
+PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
+    android.hidl.manager@1.0 \
+    android.hidl.base@1.0.vendor \
+    android.hidl.manager@1.0.vendor
+
 # Theme Picker
 PRODUCT_PACKAGES += \
     ThemePicker
