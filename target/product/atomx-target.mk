ca3be360125808168403b4e80f6d1b53af6f8bf2
Vishalcj17 <vishalcj17@gmail.com>
2021-08-19 11:34:39 +0530

    AtomX: Bring-in QTI Privapp and whitelist permissions
    Change-Id: If8fd3adda8fd062f215d05873dd6cf21f74f88a5

--- target/product/atomx-target.mk
+++ target/product/atomx-target.mk
@@ -29,3 +29,8 @@ $(call inherit-product, vendor/atomx/target/product/properties.mk)
 
 # Move Wi-Fi modules to vendor
 PRODUCT_VENDOR_MOVE_ENABLED := true
+
+# Permissions
+PRODUCT_COPY_FILES += \
+    vendor/atomx/target/config/permissions/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml \
+    vendor/atomx/target/config/permissions/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml
