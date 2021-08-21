750be1808c1da52d50ceb7ca2bbf63386805e09c
Vishalcj17 <vishalcj17@gmail.com>
2021-08-21 22:51:29 +0530

    AtomX: board: Disable EAP Proxy
    • Breaks compilation due to dependence on proprietary headers.
    
    Change-Id: I9690c620998104dceef70c4807a17875859a5e74

--- target/board/BoardConfigAtomX.mk
+++ target/board/BoardConfigAtomX.mk
@@ -4,6 +4,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Disable EAP Proxy because it depends on proprietary headers
+# and breaks WPA Supplicant compilation.
+DISABLE_EAP_PROXY := true
+
 # Include our Qualcomm Board configuration.
 include device/qcom/common/BoardConfigQcom.mk
 
