c8375ff9974c9d41d284a5ad8840d24b4a441067
Danny Lin <danny@kdrag0n.dev>
2021-10-09 06:24:31 -0700

    AtomX: properties: Enable one-handed mode
     * There's nothing device-specific about this feature,
       so enable it for everyone.
    
    Change-Id: I4734578f00e2f9f11a7e2e6c9bf86850830c5352

--- target/product/properties.mk
+++ target/product/properties.mk
@@ -27,3 +27,7 @@ PRODUCT_SYSTEM_EXT_PROPERTIES += \
 # Enable IORap I/O Prefetching
 PRODUCT_SYSTEM_PROPERTIES += \
     persist.device_config.runtime_native_boot.iorap_perfetto_enable=true
+
+# Enable one-handed mode
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.support_one_handed_mode=true
