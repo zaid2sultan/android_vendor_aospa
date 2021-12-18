00a307c0ba03f4eecd684b244107bbdf687c730e
wHo-EM-i <ehteshammalik18998@gmail.com>
2021-12-18 19:46:40 +0000

    atomX: products: Introduce Lavender Family
    Signed-off-by: wHo-EM-i <ehteshammalik18998@gmail.com>

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -7,9 +7,11 @@
 # Product Makefiles
 PRODUCT_MAKEFILES += \
     $(LOCAL_DIR)/X00TD/atomx_X00TD.mk \
-    $(LOCAL_DIR)/X01BD/atomx_X01BD.mk
+    $(LOCAL_DIR)/X01BD/atomx_X01BD.mk \
+    $(LOCAL_DIR)/lavender/atomx_lavender.mk
 
 # Lunch targets
 COMMON_LUNCH_CHOICES += \
     atomx_X00TD-userdebug \
-    atomx_X01BD-userdebug
+    atomx_X01BD-userdebug \
+    atomx_lavender-user
