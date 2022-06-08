e5bbd6ed1ab0e52680de0de517178378d0784f28
Chris Crump <chriscrawford893@gmail.com>
2022-06-08 01:12:15 -0400

    AtomX: config: Move APN/SPN configurations from QTI telephony component
    These are needed for all devices and should not be Qualcomm specific
    
    Change-Id: I6fea2c2dce95a8fd9f260e5187d8a91f46e0d4fe

--- target/product/atomx-target.mk
+++ target/product/atomx-target.mk
@@ -79,6 +79,11 @@ PRODUCT_COPY_FILES += \
     vendor/atomx/target/config/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml \
     vendor/atomx/target/config/permissions/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml
 
+# Sensitive phone numbers and APN configurations
+PRODUCT_COPY_FILES += \
+    vendor/atomx/target/config/apns-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml \
+    vendor/atomx/target/config/sensitive_pn.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sensitive_pn.xml
+
 # Set compiler filter "verify" and disable AOT-compilation in dexpreopt
 RELAX_USES_LIBRARY_CHECK := true
 
